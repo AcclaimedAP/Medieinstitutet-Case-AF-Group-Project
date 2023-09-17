f3f516b66dc01c994fc168830ec5fb7af6ea41ed
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-09-17 22:57:55 +0200

    Added/replaced AF component

--- src/views/Results.tsx
+++ src/views/Results.tsx
@@ -1,7 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import IOccupations from "../interfaces/IOccupations";
 import { OccupationContext } from "../OccupationsContext";
-
+import { DigiExpandableAccordion, DigiLayoutBlock, DigiTypography } from "@digi/arbetsformedlingen-react";
+import { LayoutBlockVariation, TypographyVariation } from "@digi/arbetsformedlingen";
 
 const SearchResults = () => {
     const context = useContext(OccupationContext);
@@ -15,19 +16,18 @@ const SearchResults = () => {
     
     function OccupationMap(occupationsList: IOccupations) {
 
-        return occupationsList.related_occupations.map(occupation => 
-            
-            <div key={occupation.id} className="bg-slate-100 p-5 my-3">
-                <details className="border-solid border-2 border-slate-300 rounded">
-                    <summary className="m-2 p-5">
-                        <h2 className="inline">{occupation.occupation_label}</h2>
-                    </summary>
+        return occupationsList.related_occupations.map(occupation =>     
+                <DigiExpandableAccordion 
+                    afHeading={occupation.occupation_label}
+                    key={occupation.id}
+                >
                     <div className="m-5">
-                        <h3>taxonomi ID: {occupation.concept_taxonomy_id}</h3>
-                        <h4>Arbetsgrupp: {occupation.occupation_group.occupation_group_label}</h4>
-                    </div>
-                </details>
-            </div>
+                        <DigiTypography>
+                            <p>taxonomi ID: {occupation.concept_taxonomy_id}</p>
+                            <p>Arbetsgrupp: {occupation.occupation_group.occupation_group_label}</p>
+                        </DigiTypography>
+                        </div>
+                </DigiExpandableAccordion>
             
         )
     }
@@ -41,13 +41,17 @@ const SearchResults = () => {
     
     return (
         <>
-            <div className="bg-slate-200">
-                <h1>
-                    Sökresultat, hittade {occupations.hits_total}st, visar {occupations.hits_returned}st
-                </h1>
-            
+            <DigiLayoutBlock
+                afVariation={LayoutBlockVariation.SECONDARY}>
+                <DigiTypography
+                afVariation={TypographyVariation.SMALL}
+                >
+                    <h1>
+                        Sökresultat, hittade {occupations.hits_total}st, visar {occupations.hits_returned}st
+                    </h1>
+                </DigiTypography>
                 <OccupationMap {...occupations}></OccupationMap>
-            </div>
+            </DigiLayoutBlock>
         </>
     )
 }
