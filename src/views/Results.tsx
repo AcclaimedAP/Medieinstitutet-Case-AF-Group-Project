70069b80e76f9ed2e2fc27eaeee5a8b0ed708e63
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-09-17 22:38:46 +0200

    added occupation info and light formatting

--- src/views/Results.tsx
+++ src/views/Results.tsx
@@ -17,12 +17,19 @@ const SearchResults = () => {
 
         return occupationsList.related_occupations.map(occupation => 
             
-                <div key={occupation.id}>
-                    <h2>{occupation.occupation_label}</h2>
-                    <h3>{ occupation.id }</h3>
-                </div>
+            <div key={occupation.id} className="bg-slate-100 p-5 my-3">
+                <details className="border-solid border-2 border-slate-300 rounded">
+                    <summary className="m-2 p-5">
+                        <h2 className="inline">{occupation.occupation_label}</h2>
+                    </summary>
+                    <div className="m-5">
+                        <h3>taxonomi ID: {occupation.concept_taxonomy_id}</h3>
+                        <h4>Arbetsgrupp: {occupation.occupation_group.occupation_group_label}</h4>
+                    </div>
+                </details>
+            </div>
             
-            )
+        )
     }
 
 
@@ -34,8 +41,13 @@ const SearchResults = () => {
     
     return (
         <>
-            <div>Sökresultat</div>
-            <OccupationMap {...occupations}></OccupationMap>
+            <div className="bg-slate-200">
+                <h1>
+                    Sökresultat, hittade {occupations.hits_total}st, visar {occupations.hits_returned}st
+                </h1>
+            
+                <OccupationMap {...occupations}></OccupationMap>
+            </div>
         </>
     )
 }
