73441489e3e86b531401b913a6948781f89b3dc0
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-18 15:54:03 +0200

    update interface, add interface enrichedCompetence

--- src/interfaces/IRelatedOccupation.ts
+++ src/interfaces/IRelatedOccupation.ts
@@ -1,13 +1,23 @@
 interface IRelatedOccupation {
-  concept_taxonomy_id: string,
-  id: string,
-  legacy_ams_taxonomy_id: string,
+  concept_taxonomy_id: string;
+  id: string;
+  legacy_ams_taxonomy_id: string;
+  metadata: {
+    enriched_candidates_term_frequency: {
+      competencies: IEnrichedCompetence[];
+    };
+  };
   occupation_group: {
-    concept_taxonomy_id: string,
-    occupation_group_label: string,
-    ssyk: string,
-  },
-  occupation_label: string
+    concept_taxonomy_id: string;
+    occupation_group_label: string;
+    ssyk: string;
+  };
+  occupation_label: string;
+}
+
+export interface IEnrichedCompetence {
+  term: string;
+  percent_for_occupation: number;
 }
 
 export default IRelatedOccupation;
