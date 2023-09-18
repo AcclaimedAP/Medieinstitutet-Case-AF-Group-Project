5c8c39d24d123f77cae8b10c78c601de67d02522
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-18 15:54:40 +0200

    refactor results to use occupationAccordion comp

--- src/views/Results.tsx
+++ src/views/Results.tsx
@@ -1,63 +1,50 @@
 import { useContext, useEffect, useState } from 'react';
 import IOccupations from '../interfaces/IOccupations';
 import { OccupationContext } from '../OccupationsContext';
-import { DigiExpandableAccordion, DigiLayoutBlock, DigiTypography } from '@digi/arbetsformedlingen-react';
-import { LayoutBlockVariation, TypographyVariation } from '@digi/arbetsformedlingen';
-import { EduToWorkData } from "../service/EduToWorkService";
+import {
+  DigiLayoutBlock,
+  DigiTypography,
+} from '@digi/arbetsformedlingen-react';
+import {
+  LayoutBlockVariation,
+  TypographyVariation,
+} from '@digi/arbetsformedlingen';
+import OccupationAccordion from '../components/OccupationAccordion';
 
 const SearchResults = () => {
   const context = useContext(OccupationContext);
   const [occupations, setOccupations] = useState<IOccupations>();
-  const searchService = EduToWorkData();
 
   useEffect(() => {
     const updateOccupations = () => {
       setOccupations(context?.state.occupations);
-    }
+    };
     updateOccupations();
   });
 
-  const searchCompetencies = async (taxonomyId: string) => {
-    const result = await searchService.fetchCompetencies(taxonomyId);
-    console.log(result)
-  };
-    
   function OccupationMap(occupationsList: IOccupations) {
-    return occupationsList.related_occupations.map(occupation =>     
-      <DigiExpandableAccordion 
-        afHeading={occupation.occupation_label}
-        key={occupation.id}
-        onClick={() => searchCompetencies(occupation.concept_taxonomy_id)}
-      >
-        <div className='m-5'>
-          <DigiTypography>
-            <p>taxonomi ID: {occupation.concept_taxonomy_id}</p>
-            <p>Arbetsgrupp: {occupation.occupation_group.occupation_group_label}</p>
-          </DigiTypography>
-        </div>
-      </DigiExpandableAccordion>
-    )
+    return occupationsList.related_occupations.map((occupation, index) => (
+      <OccupationAccordion occupation={occupation} key={index} />
+    ));
   }
 
   if (!occupations) {
     return null;
   }
-    
+
   return (
     <>
-      <DigiLayoutBlock
-        afVariation={LayoutBlockVariation.SECONDARY}>
-        <DigiTypography
-        afVariation={TypographyVariation.SMALL}
-        >
+      <DigiLayoutBlock afVariation={LayoutBlockVariation.SECONDARY}>
+        <DigiTypography afVariation={TypographyVariation.SMALL}>
           <h1>
-            Sökresultat, hittade {occupations.hits_total}st, visar {occupations.hits_returned}st
+            Sökresultat, hittade {occupations.hits_total}st, visar{' '}
+            {occupations.hits_returned}st
           </h1>
         </DigiTypography>
         <OccupationMap {...occupations}></OccupationMap>
       </DigiLayoutBlock>
     </>
-  )
-}
+  );
+};
 
 export default SearchResults;
