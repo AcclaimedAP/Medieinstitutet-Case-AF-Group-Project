83e8e0c828ee64e0d9b22f05e067bb8105334f16
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-20 19:29:01 +0200

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00005A',
+        accent: '#95C23E',
+      },
+    },
+
     screens: {
       tablet: '640px',
       laptop: '1024px',
     },
   },
+
   plugins: [],
 };
