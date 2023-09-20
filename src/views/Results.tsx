3034181d286130ec7ccec513038d0d7d2e8b77ac
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-09-20 17:37:59 +0200

    fix to searching twice -> removed digiblock

--- src/views/Results.tsx
+++ src/views/Results.tsx
@@ -1,31 +1,31 @@
-import { useContext, useEffect, useState } from 'react';
-import IOccupations from '../interfaces/IOccupations';
-import { OccupationContext } from '../OccupationsContext';
-import {
-  DigiLayoutBlock,
-  DigiTypography,
-} from '@digi/arbetsformedlingen-react';
-import {
-  LayoutBlockVariation,
-  TypographyVariation,
-} from '@digi/arbetsformedlingen';
-import OccupationAccordion from '../components/OccupationAccordion';
+import { useContext, useEffect, useState } from "react";
+import IOccupations from "../interfaces/IOccupations";
+import { OccupationContext } from "../OccupationsContext";
+import { DigiLayoutBlock, DigiTypography } from "@digi/arbetsformedlingen-react";
+import { LayoutBlockVariation, TypographyVariation } from "@digi/arbetsformedlingen";
+import OccupationAccordion from "../components/OccupationAccordion";
 
 const SearchResults = () => {
   const context = useContext(OccupationContext);
   const [occupations, setOccupations] = useState<IOccupations>();
 
   useEffect(() => {
-    const updateOccupations = () => {
+    const updateOccupations = async () => {
       setOccupations(context?.state.occupations);
     };
     updateOccupations();
   });
 
   function OccupationMap(occupationsList: IOccupations) {
-    return occupationsList.related_occupations.map((occupation, index) => (
-      <OccupationAccordion occupation={occupation} key={index} />
-    ));
+    return (
+      <>
+        {occupationsList.related_occupations.map((occupation, index) => (
+          <div key={index}>
+            <OccupationAccordion occupation={occupation} key={index} />
+          </div>
+        ))}
+      </>
+    );
   }
 
   if (!occupations) {
@@ -34,15 +34,14 @@ const SearchResults = () => {
 
   return (
     <>
-      <DigiLayoutBlock afVariation={LayoutBlockVariation.SECONDARY}>
+      <div>
         <DigiTypography afVariation={TypographyVariation.SMALL}>
           <h1>
-            Sökresultat, hittade {occupations.hits_total}st, visar{' '}
-            {occupations.hits_returned}st
+            Sökresultat, hittade {occupations.hits_total}st, visar {occupations.hits_returned}st
           </h1>
         </DigiTypography>
         <OccupationMap {...occupations}></OccupationMap>
-      </DigiLayoutBlock>
+      </div>
     </>
   );
 };
