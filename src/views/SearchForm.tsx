001d0498c3d0c1eb4340a668ce60561fd9ddbdde
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-09-22 16:16:35 +0200

    fix object wrong property

--- src/views/SearchForm.tsx
+++ src/views/SearchForm.tsx
@@ -1,29 +1,17 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import {
-  DigiButton,
-  DigiFormTextarea,
-  DigiFormInput,
-} from '@digi/arbetsformedlingen-react';
-import {
-  ButtonVariation,
-  FormInputType,
-  FormInputValidation,
-  FormInputVariation,
-  FormTextareaValidation,
-  FormTextareaVariation,
-} from '@digi/arbetsformedlingen';
-import { EduToWorkData } from '../service/EduToWorkService';
-import IOccupations from '../interfaces/IOccupations';
-import { OccupationDispatchContext } from '../OccupationsContext';
-
+import { DigiButton, DigiFormTextarea, DigiFormInput } from "@digi/arbetsformedlingen-react";
+import { ButtonVariation, FormInputType, FormInputValidation, FormInputVariation, FormTextareaValidation, FormTextareaVariation } from "@digi/arbetsformedlingen";
+import { EduToWorkData } from "../service/EduToWorkService";
+import IOccupations from "../interfaces/IOccupations";
+import { OccupationDispatchContext } from "../OccupationsContext";
 
 const SearchForm = () => {
   const dispatch = useContext(OccupationDispatchContext);
   const searchService = EduToWorkData();
-  const [headlineInput, setHeadlineInput] = useState<string>('');
-  const [textInput, setTextInput] = useState<string>('');
-  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [headlineInput, setHeadlineInput] = useState<string>("");
+  const [textInput, setTextInput] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
   const [isButtonClicked, setIsButtonClicked] = useState(false);
   const navigate = useNavigate();
 
@@ -54,10 +42,10 @@ const SearchForm = () => {
       headlineInput: headlineInput,
       textInput: textInput,
     };
-    
-    dispatch({ payload: result, type: "updated" });
-    navigate('/search');
-    
+
+    dispatch({ payload, type: "updated" });
+    navigate("/search");
+
     const id = searchParams.get("id") || "";
 
     setIsButtonClicked(true);
@@ -66,42 +54,17 @@ const SearchForm = () => {
 
   return (
     <>
-      <div className={`laptop:bg-desktopBackground laptop:bg-cover laptop:h-[calc(100vh-80px)] ${isButtonClicked ? 'laptop:h-screen laptop:bg-fixed' : ''}`}>
-        <div
-          className={`bg-primary min-w-[320px] p-6 gap-4 h-auto flex flex-col items-center form laptop:w-1/3 laptop:rounded-lg laptop:fixed laptop:mt-260 ${
-            isButtonClicked
-              ? 'laptop:left-[100px]'
-              : 'laptop:left-1/2 laptop:transform laptop:-translate-x-1/2'
-          } laptop:-translate-y-1/2`}
-        >
-          <h2 className='text-white text-h3 pb-2 laptop:pb-6'>
-            Sök efter yrken utifrån utbildning
-          </h2>
-          <DigiFormInput
-            className='w-full'
-            afLabel='Utbildningstitel'
-            afVariation={FormInputVariation.MEDIUM}
-            afType={FormInputType.TEXT}
-            afValidation={FormInputValidation.NEUTRAL}
-            value={headlineInput}
-            onAfOnChange={(e) => setHeadlineInput(String(e.target.value))}
-          />
-          <DigiFormTextarea
-            className='w-full'
-            afLabel='Utbildningsbeskrivning*'
-            afVariation={FormTextareaVariation.MEDIUM}
-            afValidation={FormTextareaValidation.NEUTRAL}
-            value={textInput}
-            onAfOnChange={(e) => setTextInput(String(e.target.value))}
-          ></DigiFormTextarea>
-          {errorMessage && (
-            <div className='text-[red] mb-[10px]'>{errorMessage}</div>
-          )}
+      <div className={`laptop:bg-desktopBackground laptop:bg-cover laptop:h-[calc(100vh-80px)] ${isButtonClicked ? "laptop:h-screen laptop:bg-fixed" : ""}`}>
+        <div className={`bg-primary min-w-[320px] p-6 gap-4 h-auto flex flex-col items-center form laptop:w-1/3 laptop:rounded-lg laptop:fixed laptop:mt-260 ${isButtonClicked ? "laptop:left-[100px]" : "laptop:left-1/2 laptop:transform laptop:-translate-x-1/2"} laptop:-translate-y-1/2`}>
+          <h2 className="text-white text-h3 pb-2 laptop:pb-6">Sök efter yrken utifrån utbildning</h2>
+          <DigiFormInput className="w-full" afLabel="Utbildningstitel" afVariation={FormInputVariation.MEDIUM} afType={FormInputType.TEXT} afValidation={FormInputValidation.NEUTRAL} value={headlineInput} onAfOnChange={(e) => setHeadlineInput(String(e.target.value))} />
+          <DigiFormTextarea className="w-full" afLabel="Utbildningsbeskrivning*" afVariation={FormTextareaVariation.MEDIUM} afValidation={FormTextareaValidation.NEUTRAL} value={textInput} onAfOnChange={(e) => setTextInput(String(e.target.value))}></DigiFormTextarea>
+          {errorMessage && <div className="text-[red] mb-[10px]">{errorMessage}</div>}
           <DigiButton
-            className='border-2 border-accent rounded-lg'
+            className="border-2 border-accent rounded-lg"
             onAfOnClick={() => {
-          workTitles(headlineInput, textInput);
-          }}
+              workTitles(headlineInput, textInput);
+            }}
             afVariation={ButtonVariation.PRIMARY}
           >
             Sök
