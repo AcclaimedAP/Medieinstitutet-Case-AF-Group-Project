58518c094aafb43e3bca9c1273c20150b7810626
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-25 16:52:26 +0200

    add page to search params

--- src/components/OccupationAccordion.tsx
+++ src/components/OccupationAccordion.tsx
@@ -1,11 +1,16 @@
-import { DigiExpandableAccordion, DigiTypography } from "@digi/arbetsformedlingen-react";
-import IRelatedOccupation, { IEnrichedCompetence } from "../interfaces/IRelatedOccupation";
-import Diagram from "./Diagram";
-import { useEffect, useState } from "react";
-import { EduToWorkData } from "../service/EduToWorkService";
-import LoadingSpinner from "./LoadingSpinner";
-import { TypographyVariation } from "@digi/arbetsformedlingen";
-import { useSearchParams } from "react-router-dom";
+import {
+  DigiExpandableAccordion,
+  DigiTypography,
+} from '@digi/arbetsformedlingen-react';
+import IRelatedOccupation, {
+  IEnrichedCompetence,
+} from '../interfaces/IRelatedOccupation';
+import Diagram from './Diagram';
+import { useEffect, useState } from 'react';
+import { EduToWorkData } from '../service/EduToWorkService';
+import LoadingSpinner from './LoadingSpinner';
+import { TypographyVariation } from '@digi/arbetsformedlingen';
+import { useSearchParams } from 'react-router-dom';
 
 interface IProps {
   occupation: IRelatedOccupation;
@@ -14,7 +19,7 @@ interface IProps {
 const OccupationAccordion = ({ occupation }: IProps) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const idParamsCheck = () => {
-    return searchParams.get("id") == occupation.id;
+    return searchParams.get('id') == occupation.id;
   };
   const [diagramData, setDiagramData] = useState<IEnrichedCompetence[]>([]);
   const [isExpanded, setIsExpanded] = useState<boolean>(idParamsCheck());
@@ -23,7 +28,9 @@ const OccupationAccordion = ({ occupation }: IProps) => {
   useEffect(() => {
     if (!isExpanded) return;
 
-    const searchCompetencies = async (taxonomyId: string): Promise<IRelatedOccupation> => {
+    const searchCompetencies = async (
+      taxonomyId: string
+    ): Promise<IRelatedOccupation> => {
       const result = await searchService.fetchCompetencies(taxonomyId);
 
       return result;
@@ -31,16 +38,26 @@ const OccupationAccordion = ({ occupation }: IProps) => {
 
     if (!diagramData.length) {
       searchCompetencies(occupation.concept_taxonomy_id)
-        .then((res) => setDiagramData(res.metadata.enriched_candidates_term_frequency.competencies.slice(0, 10)))
+        .then((res) =>
+          setDiagramData(
+            res.metadata.enriched_candidates_term_frequency.competencies.slice(
+              0,
+              10
+            )
+          )
+        )
         .catch((err) => console.error(err));
     }
   }, [isExpanded]);
 
-  const handleAccordionClick: React.MouseEventHandler<HTMLDigiExpandableAccordionElement> = () => {
+  const handleAccordionClick: React.MouseEventHandler<
+    HTMLDigiExpandableAccordionElement
+  > = () => {
     const params = {
-      title: searchParams.get("title") || "",
-      desc: searchParams.get("desc") || "",
-      id: searchParams.get("id") || "",
+      title: searchParams.get('title') || '',
+      desc: searchParams.get('desc') || '',
+      page: searchParams.get('page') || '',
+      id: searchParams.get('id') || '',
     };
     setIsExpanded((prev) => !prev);
     if (!params.title || !params.desc) {
@@ -64,13 +81,25 @@ const OccupationAccordion = ({ occupation }: IProps) => {
 
   return (
     <>
-      <DigiExpandableAccordion afHeading={occupation.occupation_label} onClick={handleAccordionClick} afExpanded={idParamsCheck()}>
-        <div className="m-5 flex flex-col gap-4">
+      <DigiExpandableAccordion
+        afHeading={occupation.occupation_label}
+        onClick={handleAccordionClick}
+        afExpanded={idParamsCheck()}
+      >
+        <div className='m-5 flex flex-col gap-4'>
           <DigiTypography afVariation={TypographyVariation.SMALL}>
             <p>taxonomi ID: {occupation.concept_taxonomy_id}</p>
-            <p>Arbetsgrupp: {occupation.occupation_group.occupation_group_label}</p>
+            <p>
+              Arbetsgrupp: {occupation.occupation_group.occupation_group_label}
+            </p>
           </DigiTypography>
-          <div className="w-[300px] h-[400px] tablet:w-[500px] tablet:h-[500px]">{diagramData.length ? <Diagram diagramData={diagramData} /> : <LoadingSpinner />}</div>
+          <div className='w-[300px] h-[400px] tablet:w-[500px] tablet:h-[500px]'>
+            {diagramData.length ? (
+              <Diagram diagramData={diagramData} />
+            ) : (
+              <LoadingSpinner />
+            )}
+          </div>
         </div>
       </DigiExpandableAccordion>
     </>
--- src/views/Results.tsx
+++ src/views/Results.tsx
@@ -9,21 +9,47 @@ import { TypographyVariation } from '@digi/arbetsformedlingen';
 import OccupationAccordion from '../components/OccupationAccordion';
 import { EduToWorkData } from '../service/EduToWorkService';
 import { calculatePages } from '../utilities/CalculatePagination';
+import { useSearchParams } from 'react-router-dom';
 
 const SearchResults = () => {
   const context = useContext(OccupationContext);
-  const [occupations, setOccupations] = useState<IOccupations>();
   const searchService = EduToWorkData();
   const dispatch = useContext(OccupationDispatchContext);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
-    const updateOccupations = () => {
-      setOccupations(context?.state.occupations);
-    };
-    updateOccupations();
-  });
+    const fetchData = async () => {
+      const params = {
+        title: searchParams.get('title'),
+        desc: searchParams.get('desc'),
+        page: searchParams.get('page'),
+      };
 
-  const fetchMoreOccupations = async (page: number) => {
+      if (params.title && params.desc) {
+        const result = await searchService.fetchWorkTitles(
+          params.title!,
+          params.desc!,
+          (Number(params.page) - 1) * 10
+        );
+
+        const payload = {
+          occupations: result,
+          headlineInput: params.title,
+          textInput: params.desc,
+        };
+
+        dispatch({ payload, type: 'updated' });
+      }
+    };
+    fetchData();
+  }, [
+    searchParams.get('title'),
+    searchParams.get('desc'),
+    searchParams.get('page'),
+  ]);
+
+  const changePage = async (page: number) => {
+    console.log(page);
     const result: IOccupations = await searchService.fetchWorkTitles(
       String(context?.state.headlineInput),
       String(context?.state.textInput),
@@ -37,6 +63,12 @@ const SearchResults = () => {
     };
 
     dispatch({ payload, type: 'updated' });
+
+    setSearchParams({
+      title: payload.headlineInput,
+      desc: payload.textInput,
+      page: (page + 1).toString(),
+    });
   };
 
   function OccupationMap(occupationsList: IOccupations) {
@@ -61,6 +93,7 @@ const SearchResults = () => {
                   Number(searchParams.get('page')) === index + 1 &&
                   '!bg-primary !text-whiteDark'
                 }`}
+                onClick={() => changePage(index)}
               >
                 {index + 1}
               </button>
